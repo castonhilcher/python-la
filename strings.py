fe43da940941d6759bf971dd5c867e96a98dac18
Caston Hilcher <castonhilcher@gmail.com>
2020-02-18 08:27:09 -0600

    Strings.py methods added

---
+++ strings.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    # String is a sequence of text -> using single quotes and double quotes
+    print('single' + ' quote')
+    print('Ha' * 4)  # HaHaHaHa
+    print('Single'.find('d'))  # -1 it could not find it
+    print('Single'.find('g'))  # will return 3 (index of found)
+    print('Testing'.lower())  # will return testing
+    print('Hello\dfd')
+    print('Hello\\dfd')
+    print('Hello\ndfd')
+    print('"Double quotes"')
+    print('\'Single\' quotes')
+    # String method signatures - https://docs.python.org/3/library/stdtypes.html#text-sequence-type-strl
+
